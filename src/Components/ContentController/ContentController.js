d3f33d18006fe50ee0fda5ee05b56e110a2f6ab0
Adam Mornes <adammornes@gmail.com>
2017-11-16 15:02:23 -0600

    Added controller for main content sections

---
+++ src/Components/ContentController/ContentController.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ContentController extends Component {
+  render() {
+    return (
+      <div className="content-wrapper">
+
+      </div>
+    )
+  }
+}
+
+export default ContentController;
--- src/index.js
+++ src/index.js
@@ -4,7 +4,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles.scss';
 import Header from './Components/Header/Header';
+import ContentController from './Components/ContentController/ContentController';
 import Footer from './Components/Footer/Footer';
 
 ReactDOM.render(<Header />, document.getElementById('Header'));
+ReactDOM.render(<ContentController />, document.getElementById('Content'));
 ReactDOM.render(<Footer />, document.getElementById('Footer'));
