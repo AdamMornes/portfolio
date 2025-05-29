b4999877d080b17ecaa41adc838fa4dc6f336001
Adam Mornes <adammornes@gmail.com>
2025-05-29 18:21:08 -0400

    header desktop styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,22 +2,41 @@ import Link from 'next/link';
 
 export default function Header() {
   return (
-    <header>
-      <Link href="/">Adam Mornes</Link>
+    <header className="fixed top-0 left-0 right-0 font-raleway py-7 px-16">
+      <div className="flex justify-between items-center">
+        <Link className="text-3xl text-white" href="/">
+          Adam Mornes
+        </Link>
 
-      <nav>
-        <ul>
-          <li>
-            <Link href="/about">About</Link>
-          </li>
-          <li>
-            <Link href="/projects">Projects</Link>
-          </li>
-          <li>
-            <Link href="/contact">Contact</Link>
-          </li>
-        </ul>
-      </nav>
+        <nav>
+          <ul className="flex gap-20">
+            <li>
+              <Link
+                className="text-2xl text-white hover:underline"
+                href="/about"
+              >
+                About
+              </Link>
+            </li>
+            <li>
+              <Link
+                className="text-2xl text-white hover:underline"
+                href="/projects"
+              >
+                Projects
+              </Link>
+            </li>
+            <li>
+              <Link
+                className="text-2xl text-white hover:underline"
+                href="/contact"
+              >
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 }
