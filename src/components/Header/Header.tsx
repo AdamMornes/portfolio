8e0566dcb36e9fef2038dfa4de5e49befb8df68b
Adam Mornes <adammornes@gmail.com>
2025-05-29 20:46:22 -0400

    Header styles

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,10 @@
 import HeaderLogo from './HeaderLogo';
 import HeaderNav from './HeaderNav';
+import styles from './Header.module.css';
 
 export default function Header() {
   return (
-    <header className="fixed top-0 left-0 right-0 bg-background font-raleway py-7 px-16 z-10">
+    <header className={styles.header + ' ' + styles.minimized}>
       <div className="flex justify-between items-center">
         <HeaderLogo />
         <HeaderNav />
