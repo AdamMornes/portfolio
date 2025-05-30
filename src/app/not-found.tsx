21a35ac0aa7983b1a274ad84cde584d7f1b6b94b
Adam Mornes <adammornes@gmail.com>
2025-05-30 11:13:02 -0400

    404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+      <h1 className="text-2xl font-bold">404</h1>
+      <div className="mx-4 h-8 w-px bg-foreground"></div>
+      <p>Page Not Found</p>
+    </div>
+  );
+}
