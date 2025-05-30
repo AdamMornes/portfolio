8fe4bdfb89c70c4bec929b8f0bdbb3d585d34766
Adam Mornes <adammornes@gmail.com>
2025-05-29 20:28:49 -0400

    header bg and z-index

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,7 +3,7 @@ import HeaderNav from './HeaderNav';
 
 export default function Header() {
   return (
-    <header className="fixed top-0 left-0 right-0 font-raleway py-7 px-16">
+    <header className="fixed top-0 left-0 right-0 bg-background font-raleway py-7 px-16 z-10">
       <div className="flex justify-between items-center">
         <HeaderLogo />
         <HeaderNav />
