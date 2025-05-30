9bf699084b3fb96d7a180fbe3f8eddfe0679eb74
Adam Mornes <adammornes@gmail.com>
2025-05-30 09:59:07 -0400

    basic header mobile styles

--- src/components/Header/HeaderLogo.tsx
+++ src/components/Header/HeaderLogo.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function HeaderLogo() {
   return (
-    <Link className="text-4xl text-foreground" href="/">
+    <Link className="text-2xl text-foreground lg:text-4xl" href="/">
       Adam Mornes
     </Link>
   );
--- src/components/Header/HeaderNav.tsx
+++ src/components/Header/HeaderNav.tsx
@@ -3,17 +3,19 @@ import HeaderNavLink from './HeaderNavLink';
 export default function HeaderNav() {
   return (
     <nav>
-      <ul className="flex gap-20">
-        <li>
-          <HeaderNavLink href="/about">About</HeaderNavLink>
-        </li>
-        <li>
-          <HeaderNavLink href="/projects">Projects</HeaderNavLink>
-        </li>
-        <li>
-          <HeaderNavLink href="/contact">Contact</HeaderNavLink>
-        </li>
-      </ul>
+      <div className="hidden lg:block">
+        <ul className="flex gap-20">
+          <li>
+            <HeaderNavLink href="/about">About</HeaderNavLink>
+          </li>
+          <li>
+            <HeaderNavLink href="/projects">Projects</HeaderNavLink>
+          </li>
+          <li>
+            <HeaderNavLink href="/contact">Contact</HeaderNavLink>
+          </li>
+        </ul>
+      </div>
     </nav>
   );
 }
