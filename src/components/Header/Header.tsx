981ab0e4fc35f053c7fc8052898379b48444ba81
Adam Mornes <adammornes@gmail.com>
2025-05-30 09:36:19 -0400

    minimize header on scroll

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,24 @@
+'use client';
+
+import { useEffect, useState } from 'react';
 import HeaderLogo from './HeaderLogo';
 import HeaderNav from './HeaderNav';
 
 export default function Header() {
+  const [isMinimized, setIsMinimized] = useState(false);
+  const onScroll = () => {
+    setIsMinimized(window.scrollY > 0);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', onScroll);
+
+    return () => window.removeEventListener('scroll', onScroll);
+  }, []);
+
   return (
-    <header className="header">
-      <div className="flex justify-between items-center">
+    <header className={`header ${isMinimized ? '-minimized' : ''}`}>
+      <div className="flex justify-between items-center h-full">
         <HeaderLogo />
         <HeaderNav />
       </div>
