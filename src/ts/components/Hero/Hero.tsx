4ebcf385127c48980160ef7e34827a62bb190c27
Adam Mornes <adammornes@gmail.com>
2025-05-31 18:35:52 -0400

    Hero component

--- src/ts/components/Hero/Hero.tsx
+++ src/ts/components/Hero/Hero.tsx
@@ -1,36 +1,32 @@
 import Image from 'next/image';
+import ButtonBrand from '../Common/Buttons/ButtonBrand';
 
 export default function Hero() {
   return (
-    <div className="h-full w-full">
-      <div className="hidden md:block">
+    <div className="relative w-full h-full">
+      <div className="absolute top-0 left-0 right-0 bottom-0 z-[-1]">
         <Image
-          src="/hero-shot.jpg"
-          alt="Developer working on their computer."
-          fill
+          alt="Developer working in their office"
           className="object-cover"
+          fill
           priority={true}
+          src="/hero-shot.jpg"
+          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 1200px"
         />
       </div>
 
-      <div className="md:hidden">
-        <Image
-          src="/hero-shot-mobile.jpg"
-          alt="Developer working on their computer."
-          fill
-          className="object-cover"
-          priority={true}
-        />
-      </div>
-      <div className="absolute top-0 left-0 right-0 h-full flex text-foreground font-raleway py-24 lg:items-center">
-        <div className="container flex flex-col lg:items-end">
-          <div className="lg:pr-8">
-            <h1 className="text-3xl">
+      <div className="flex h-full text-foreground py-16 md:items-center">
+        <div className="max-w-screen-md flex items-center justify-center md:justify-end">
+          <div className="">
+            <h1 className="font-raleway text-3xl">
               Hi,
               <br />
               I&apos;m Adam Mornes.
             </h1>
-            <p className="text-3xl mt-4">I do web.</p>
+            <p className="font-raleway text-3xl mt-4">I do web.</p>
+            <ButtonBrand className="mt-4" isAnchor={true} href="/about">
+              Learn more about me
+            </ButtonBrand>
           </div>
         </div>
       </div>
