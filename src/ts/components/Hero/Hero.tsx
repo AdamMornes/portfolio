503c61061f4be5c6b7fae342153c83129452f2bb
Adam Mornes <adammornes@gmail.com>
2025-05-31 18:53:58 -0400

    more hero styling

--- src/ts/components/Hero/Hero.tsx
+++ src/ts/components/Hero/Hero.tsx
@@ -4,7 +4,7 @@ import ButtonBrand from '../Common/Buttons/ButtonBrand';
 export default function Hero() {
   return (
     <div className="hero">
-      <div className="hero__image">
+      <div className="hero__fill">
         <Image
           alt="Developer working in their office"
           className="object-cover"
@@ -15,16 +15,18 @@ export default function Hero() {
         />
       </div>
 
+      <div className="hero__fill -overlay" />
+
       <div className="hero__content">
-        <div className="hero__content-container">
-          <div className="">
+        <div className="hero__container">
+          <div className="bg-white/90 text-foreground rounded-md px-4 py-6">
             <h1 className="font-raleway text-3xl">
               Hi,
               <br />
               I&apos;m Adam Mornes.
             </h1>
             <p className="font-raleway text-3xl mt-4">I do web.</p>
-            <ButtonBrand className="mt-4" isAnchor={true} href="/about">
+            <ButtonBrand className="mt-6" isAnchor={true} href="/about">
               Learn more about me
             </ButtonBrand>
           </div>
