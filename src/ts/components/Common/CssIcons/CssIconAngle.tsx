0d6fa55099eae76702ebfa42a4b8465abdcdd1f0
Adam Mornes <adammornes@gmail.com>
2025-05-31 13:32:46 -0400

    more angle icon refactoring

--- src/ts/components/Common/CssIcons/CssIconAngle.tsx
+++ src/ts/components/Common/CssIcons/CssIconAngle.tsx
@@ -2,15 +2,21 @@ import join from '@/ts/utils/classNameJoin';
 
 type CssIconAngleProps = {
   orientation?: 'up' | 'down' | 'left' | 'right';
+  size?: 'sm' | 'lg' | 'xl' | '';
 };
 
 export default function CssIconAngle({
   orientation = 'right',
+  size = '',
 }: CssIconAngleProps) {
   return (
     <span
       aria-hidden="true"
-      className={join(['css-icon-angle', `-${orientation}`])}
+      className={join([
+        'css-icon-angle',
+        `-${orientation}`,
+        size ? `-${size}` : '',
+      ])}
     />
   );
 }
