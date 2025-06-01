152d3849d511829fcbf287f6d4159cf4541a8c4d
Adam Mornes <adammornes@gmail.com>
2025-06-01 19:40:56 -0400

    not-found design update

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,9 +1,13 @@
+import SectionHeader from '@/ts/components/Common/SectionHeader/SectionHeader';
+
 export default function NotFound() {
   return (
     <div className="flex items-center justify-center h-full">
-      <h1 className="text-2xl font-bold">404</h1>
-      <div className="mx-4 h-8 w-px bg-foreground"></div>
-      <p>Page Not Found</p>
+      <SectionHeader className="gap-0" rootElement="h1">
+        <div className="text-2xl font-bold">404</div>
+        <div className="mx-4 h-8 w-px bg-foreground"></div>
+        <div className="text-base mr-16">Page Not Found</div>
+      </SectionHeader>
     </div>
   );
 }
