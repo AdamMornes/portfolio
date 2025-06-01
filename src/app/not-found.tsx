24ce1aab916fbe65607bf1a1133a2e82baf923e5
Adam Mornes <adammornes@gmail.com>
2025-06-01 17:22:28 -0400

    not found page adjustments

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,6 +1,6 @@
 export default function NotFound() {
   return (
-    <div className="flex items-center justify-center h-screen">
+    <div className="flex items-center justify-center h-full">
       <h1 className="text-2xl font-bold">404</h1>
       <div className="mx-4 h-8 w-px bg-foreground"></div>
       <p>Page Not Found</p>
