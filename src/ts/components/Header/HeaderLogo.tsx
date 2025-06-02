fb8ddb79e9e3a3cef2bfcc046b44ce424b388a8a
Adam Mornes <adammornes@gmail.com>
2025-06-02 18:06:51 -0400

    Header styles converted to pure TailwindCss

--- src/ts/components/Header/HeaderLogo.tsx
+++ src/ts/components/Header/HeaderLogo.tsx
@@ -2,7 +2,10 @@ import Link from 'next/link';
 
 export default function HeaderLogo() {
   return (
-    <Link className="text-3xl text-foreground lg:text-4xl" href="/">
+    <Link
+      className="font-raleway text-3xl text-foreground lg:text-4xl"
+      href="/"
+    >
       Adam Mornes
     </Link>
   );
--- src/ts/components/Header/HeaderNavLink.tsx
+++ src/ts/components/Header/HeaderNavLink.tsx
@@ -1,18 +1,18 @@
 import Link, { LinkProps } from 'next/link';
 import { PropsWithChildren } from 'react';
 
-type HeaderNavLinkProps = LinkProps & {
-  className?: string;
-};
-
 export default function HeaderNavLink({
   children,
-  className,
   ...props
-}: PropsWithChildren<HeaderNavLinkProps>) {
+}: PropsWithChildren<LinkProps>) {
   return (
-    <Link className={`header-nav-link ${className}`} {...props}>
+    <Link
+      {...props}
+      className="group relative text-2xl font-raleway text-foreground"
+    >
       {children}
+
+      <div className="absolute left-0 right-0 top-8 block h-0.5 w-0 bg-foreground text-center mx-auto transition-all duration-300 group-hover:w-full" />
     </Link>
   );
 }
