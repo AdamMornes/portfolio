13736cb1c72d7cd5a323bb7392d43a2e0a957528
Adam Mornes <adammornes@gmail.com>
2025-06-05 16:54:17 -0400

    header converted to sticky

--- src/ts/components/Header/Header.tsx
+++ src/ts/components/Header/Header.tsx
@@ -1,9 +1,32 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import join from '@/ts/utils/classNameJoin';
 import HeaderLogo from './HeaderLogo';
 import HeaderNav from './HeaderNav';
 
 export default function Header() {
+  const [isMinimized, setIsMinimized] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsMinimized(window.scrollY > 0);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <header className="relative h-24 bg-background border-b border-gray-200 px-4 py-4 lg:px-16 dark:border-gray-800">
+    <header
+      className={join([
+        'sticky top-0 z-10 bg-background border-b border-gray-200 px-4 py-4 lg:px-16 dark:border-gray-800 transition-all duration-300',
+        isMinimized ? 'h-20' : 'h-24',
+      ])}
+    >
       <div className="flex justify-between items-center h-full">
         <HeaderLogo />
         <HeaderNav />
