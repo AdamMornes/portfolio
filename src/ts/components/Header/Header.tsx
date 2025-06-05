406243492cbcf4c8e2ea2fa3c5aff6512d4c022c
Adam Mornes <adammornes@gmail.com>
2025-06-05 16:57:33 -0400

    small markup adjustment to header

--- src/ts/components/Header/Header.tsx
+++ src/ts/components/Header/Header.tsx
@@ -23,13 +23,15 @@ export default function Header() {
   return (
     <header
       className={join([
-        'sticky top-0 z-10 bg-background border-b border-gray-200 px-4 py-4 lg:px-16 dark:border-gray-800 transition-all duration-300',
+        'sticky top-0 z-10 bg-background border-b border-gray-200 dark:border-gray-800 transition-all duration-300',
         isMinimized ? 'h-20' : 'h-24',
       ])}
     >
-      <div className="flex justify-between items-center h-full">
-        <HeaderLogo />
-        <HeaderNav />
+      <div className="relative h-full p-4 lg:px-16">
+        <div className="flex justify-between items-center h-full">
+          <HeaderLogo />
+          <HeaderNav />
+        </div>
       </div>
     </header>
   );
