b0544ee1d99b85ecc74c6b3085c68b4318ecdd0f
Adam Mornes <adammornes@gmail.com>
2025-06-08 17:21:19 -0400

    fix Hobbies text

--- src/ts/components/Hobbies/Hobbies.tsx
+++ src/ts/components/Hobbies/Hobbies.tsx
@@ -1,5 +1,10 @@
 import { hobbies } from '@/data/about';
 
 export default function Hobbies() {
-  return <div className="flex flex-col gap-4 mb-4">{hobbies.description}</div>;
+  return (
+    <div
+      className="flex flex-col gap-4 mb-4"
+      dangerouslySetInnerHTML={{ __html: hobbies.description }}
+    />
+  );
 }
