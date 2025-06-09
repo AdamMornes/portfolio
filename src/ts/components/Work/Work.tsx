bd5f211668b57290eff81c4d9f999858fb9848ad
Adam Mornes <adammornes@gmail.com>
2025-06-09 19:39:45 -0400

    ARI work section

--- src/ts/components/Work/Work.tsx
+++ src/ts/components/Work/Work.tsx
@@ -9,7 +9,8 @@ export default function Work() {
         classNameImageWrapper="flex flex-col"
         image={{
           alt: 'XCentium',
-          className: 'bg-white rounded-md p-4 shrink-0',
+          className:
+            'bg-white rounded-md border border-gray-200 rounded-md p-4 dark:border-gray-800',
           height: 35,
           src: '/xc-logo.webp',
           width: 200,
@@ -43,6 +44,50 @@ export default function Work() {
             Design, develop, and maintenance of a Node.js Command Line
             Interface; which is used to allow rapid building and release of
             front-end architectural solutions from a choice of frameworks.
+          </li>
+        </ul>
+      </ContentImageBlock>
+
+      <hr className="border-gray-200 dark:border-gray-800 my-8" />
+
+      <ContentImageBlock
+        className="items-start"
+        classNameImageWrapper="flex flex-col"
+        image={{
+          alt: 'XCentium',
+          className:
+            'bg-white rounded-md border border-gray-200 rounded-md p-4 dark:border-gray-800',
+          height: 35,
+          src: '/ari-logo.png',
+          width: 200,
+        }}
+        imageContent={
+          <ContentBlock className="text-sm order-1 mt-4">
+            <h2 className="sr-only">XCentium</h2>
+            <p>May 2014 - Oct 2017</p>
+          </ContentBlock>
+        }
+      >
+        <ul className="list-disc pl-4 lg:pl-8">
+          <li>
+            Implemented internal tool sets using a Node.js build process
+            implementing both Angular and Vue.js.
+          </li>
+          <li>
+            Was responsible for updating and maintaining sass themes which were
+            used as a starting point for the web development team to quickly and
+            efficiently build industry standard websites.
+          </li>
+          <li>
+            Founding member of a standards, documentation, and implementation
+            team; which established a consistent standard operating procedure
+            across diverse, cross-functional teams to improve technical
+            implementations, work communication, and efficiency.
+          </li>
+          <li>
+            Utilizing Agile methodology and strong communication, I became an
+            integral part of the development of ARI&apos;s first responsive
+            platform.
           </li>
         </ul>
       </ContentImageBlock>
