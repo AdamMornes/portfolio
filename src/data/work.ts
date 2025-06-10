0d14c41c49210e03d54bf42de09e9ce8b3ef5b22
Adam Mornes <adammornes@gmail.com>
2025-06-09 20:14:37 -0400

    Work content into data format

---
+++ src/data/work.ts
@@ -0,0 +1,41 @@
+export const workHistory = [
+  {
+    heading: 'XCentium',
+    highlights: [
+      'Work with a large range of clients to build and maintain enterprise solutions on an aggressive timeline.',
+      'Architect and develop reliable, scalable, and maintainable front-end solutions with a variety of different technologies.',
+      'Extensive experience in efficient project ramp-up times; usually involving a diverse range of solutions with varying frontend technologies.',
+      'Establish and maintain strong and honest working relationships with clients, colleagues, and stakeholders.',
+      'Design, develop, and maintenance of a Node.js Command Line Interface; which is used to allow rapid building and release of front-end architectural solutions from a choice of frameworks.',
+    ],
+    endDate: 'May 2025',
+    startDate: 'March 2018',
+    workTitle: 'Senior Frontend Developer',
+    image: {
+      alt: 'XCentium',
+      backgroundColor: 'bg-white',
+      height: 35,
+      src: '/xc-logo.webp',
+      width: 200,
+    },
+  },
+  {
+    heading: 'ARI Network Services',
+    highlights: [
+      'Implemented internal tool sets using a Node.js build process implementing both Angular and Vue.js.',
+      'Was responsible for updating and maintaining sass themes which were used as a starting point for the web development team to quickly and efficiently build industry standard websites.',
+      'Founding member of a standards, documentation, and implementation team; which established a consistent standard operating procedure across diverse, cross-functional teams to improve technical implementations, work communication, and efficiency.',
+      'Utilizing Agile methodology and strong communication, I became an integral part of the development of ARI&apos;s first responsive platform.',
+    ],
+    endDate: 'Oct 2017',
+    startDate: 'May 2014',
+    workTitle: 'Frontend Developer',
+    image: {
+      alt: 'ARI Network Services',
+      backgroundColor: 'bg-white',
+      height: 35,
+      src: '/ari-logo.png',
+      width: 200,
+    },
+  },
+];
--- src/ts/components/Work/Work.tsx
+++ src/ts/components/Work/Work.tsx
@@ -1,96 +1,47 @@
+import { workHistory } from '@/data/work';
 import ContentBlock from '../Common/ContentImageBlock/ContentBlock';
 import ContentImageBlock from '../Common/ContentImageBlock/ContentImageBlock';
+import join from '@/ts/utils/classNameJoin';
+import { Fragment } from 'react';
 
 export default function Work() {
   return (
     <ContentBlock>
-      <ContentImageBlock
-        className="items-start"
-        classNameImageWrapper="flex flex-col"
-        image={{
-          alt: 'XCentium',
-          className:
-            'bg-white rounded-md border border-gray-200 rounded-md p-4 dark:border-gray-800',
-          height: 35,
-          src: '/xc-logo.webp',
-          width: 200,
-        }}
-        imageContent={
-          <ContentBlock className="text-sm order-1 mt-4">
-            <h2 className="sr-only">XCentium</h2>
-            <p>March 2018 - May 2025</p>
-          </ContentBlock>
-        }
-      >
-        <ul className="list-disc pl-4 lg:pl-8">
-          <li>
-            Work with a large range of clients to build and maintain enterprise
-            solutions on an aggressive timeline.
-          </li>
-          <li>
-            Architect and develop reliable, scalable, and maintainable front-end
-            solutions with a variety of different technologies.
-          </li>
-          <li>
-            Extensive experience in efficient project ramp-up times; usually
-            involving a diverse range of solutions with varying frontend
-            technologies.
-          </li>
-          <li>
-            Establish and maintain strong and honest working relationships with
-            clients, colleagues, and stakeholders.
-          </li>
-          <li>
-            Design, develop, and maintenance of a Node.js Command Line
-            Interface; which is used to allow rapid building and release of
-            front-end architectural solutions from a choice of frameworks.
-          </li>
-        </ul>
-      </ContentImageBlock>
+      {workHistory.map((work, index) => (
+        <Fragment key={index}>
+          <ContentImageBlock
+            className="items-start"
+            classNameImageWrapper="flex flex-col"
+            image={{
+              alt: work.image.alt,
+              className: join([
+                'rounded-md border border-gray-200 rounded-md p-4 dark:border-gray-800',
+                work.image.backgroundColor,
+              ]),
+              height: work.image.height,
+              src: work.image.src,
+              width: work.image.width,
+            }}
+            imageContent={
+              <div className="order-1 mt-4">
+                <h2 className="sr-only">{work.heading}</h2>
+                <p className="mb-1">{work.workTitle}</p>
+                <p className="text-sm">
+                  {work.startDate} - {work.endDate}
+                </p>
+              </div>
+            }
+          >
+            <ul className="list-disc pl-4 lg:pl-8">
+              {work.highlights.map((highlight) => (
+                <li key={highlight}>{highlight}</li>
+              ))}
+            </ul>
+          </ContentImageBlock>
 
-      <hr className="border-gray-200 dark:border-gray-800 my-8" />
-
-      <ContentImageBlock
-        className="items-start"
-        classNameImageWrapper="flex flex-col"
-        image={{
-          alt: 'XCentium',
-          className:
-            'bg-white rounded-md border border-gray-200 rounded-md p-4 dark:border-gray-800',
-          height: 35,
-          src: '/ari-logo.png',
-          width: 200,
-        }}
-        imageContent={
-          <ContentBlock className="text-sm order-1 mt-4">
-            <h2 className="sr-only">XCentium</h2>
-            <p>May 2014 - Oct 2017</p>
-          </ContentBlock>
-        }
-      >
-        <ul className="list-disc pl-4 lg:pl-8">
-          <li>
-            Implemented internal tool sets using a Node.js build process
-            implementing both Angular and Vue.js.
-          </li>
-          <li>
-            Was responsible for updating and maintaining sass themes which were
-            used as a starting point for the web development team to quickly and
-            efficiently build industry standard websites.
-          </li>
-          <li>
-            Founding member of a standards, documentation, and implementation
-            team; which established a consistent standard operating procedure
-            across diverse, cross-functional teams to improve technical
-            implementations, work communication, and efficiency.
-          </li>
-          <li>
-            Utilizing Agile methodology and strong communication, I became an
-            integral part of the development of ARI&apos;s first responsive
-            platform.
-          </li>
-        </ul>
-      </ContentImageBlock>
+          <hr className="border-gray-200 dark:border-gray-800 my-4 lg:my-8 last:hidden" />
+        </Fragment>
+      ))}
     </ContentBlock>
   );
 }
