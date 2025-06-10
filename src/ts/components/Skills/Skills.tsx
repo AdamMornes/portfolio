60d9c1390e527226e48050094da038e6c45a6116
Adam Mornes <adammornes@gmail.com>
2025-06-09 21:08:04 -0400

    Skills styling

--- src/ts/components/Skills/Skills.tsx
+++ src/ts/components/Skills/Skills.tsx
@@ -9,7 +9,7 @@ export default function Skills() {
           <ul className="flex flex-wrap gap-2">
             {section.skills.map((skill) => (
               <li
-                className="bg-info-light text-gray-950 px-4 py-1 hover:scale-105 transition-all duration-100  "
+                className="border border-gray-200 px-4 py-1 transition-transform duration-200 dark:border-gray-800 hover:scale-105"
                 key={skill}
               >
                 {skill}
