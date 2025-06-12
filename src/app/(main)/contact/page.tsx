7b5946488c2411e02f627af88877fd179d7431f1
Adam Mornes <adammornes@gmail.com>
2025-06-12 19:03:11 -0400

    remove unneeded section element from Contact page

--- src/app/(main)/contact/page.tsx
+++ src/app/(main)/contact/page.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import ContactForm from '@/ts/components/ContactForm/ContactForm';
 import SectionHeader from '@/ts/components/Common/SectionHeader/SectionHeader';
-import { contactForm, meta } from '@/data/contact';
+import { meta } from '@/data/contact';
 import { siteName } from '@/data/shared';
 
 export const metadata: Metadata = {
@@ -13,10 +13,7 @@ export default function ContactPage() {
   return (
     <>
       <SectionHeader rootElement="h1">{meta.title}</SectionHeader>
-      <section>
-        <h2 className="sr-only">{contactForm.heading}</h2>
-        <ContactForm />
-      </section>
+      <ContactForm />
     </>
   );
 }
