ce0b78e42f34f0f0ad81721ca3e1bb9257c1b4ea
Adam Mornes <adammornes@gmail.com>
2025-06-13 17:04:59 -0400

    announce prop added to LoadingSpinner

--- src/ts/components/Common/LoadingSpinner/LoadingSpinner.tsx
+++ src/ts/components/Common/LoadingSpinner/LoadingSpinner.tsx
@@ -3,17 +3,22 @@ import CssIconSpinner, {
 } from '../CssIcons/CssIconSpinner';
 
 type LoadingSpinnerProps = CssIconSpinnerProps & {
+  announce?: boolean;
   loading: boolean;
   srLabel?: string;
 };
 
 export default function LoadingSpinner({
+  announce = true,
   loading,
   size = 'md',
   srLabel = 'Loading...',
 }: LoadingSpinnerProps) {
   return (
-    <span aria-live="polite" aria-busy={loading}>
+    <span
+      aria-live={announce ? 'polite' : undefined}
+      aria-busy={announce ? loading : undefined}
+    >
       {loading && (
         <>
           <span className="sr-only">{srLabel}</span>
