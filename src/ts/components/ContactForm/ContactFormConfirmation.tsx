3e4879504ce0db254ad2a21f82bf40a0aceac8c7
Adam Mornes <adammornes@gmail.com>
2025-06-13 17:05:17 -0400

    Fix missing Suspense for ContactFormConfirmation

--- src/ts/components/ContactForm/ContactFormConfirmation.tsx
+++ src/ts/components/ContactForm/ContactFormConfirmation.tsx
@@ -2,18 +2,28 @@
 
 import { contactConfirmation } from '@/data/contact';
 import { useSearchParams } from 'next/navigation';
+import { Suspense } from 'react';
+import LoadingSpinner from '../Common/LoadingSpinner/LoadingSpinner';
 
-export default function ContactFormConfirmation() {
+function ConfirmationHeading() {
   const searchParams = useSearchParams();
   const name = searchParams.get('name');
 
   return (
+    <p className="text-2xl mb-4">
+      {name
+        ? contactConfirmation.heading.replace('{name}', name)
+        : contactConfirmation.headingFallback}
+    </p>
+  );
+}
+
+export default function ContactFormConfirmation() {
+  return (
     <div>
-      <p className="text-2xl mb-4">
-        {name
-          ? contactConfirmation.heading.replace('{name}', name)
-          : contactConfirmation.headingFallback}
-      </p>
+      <Suspense fallback={<LoadingSpinner announce={false} loading={true} />}>
+        <ConfirmationHeading />
+      </Suspense>
       <p>{contactConfirmation.message}</p>
     </div>
   );
