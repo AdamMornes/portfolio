7fb989444d8d1959ff3c631320f832bbbb5bd9fc
Adam Mornes <adammornes@gmail.com>
2025-06-14 15:58:07 -0400

    Work page to use ContentCodeBlock

--- src/app/(main)/work/page.tsx
+++ src/app/(main)/work/page.tsx
@@ -3,7 +3,7 @@ import WorkHistory from '@/ts/components/WorkHistory/WorkHistory';
 import Resume from '@/ts/components/Resume/Resume';
 import Skills from '@/ts/components/Skills/Skills';
 import SectionHeader from '@/ts/components/Common/SectionHeader/SectionHeader';
-import { workHistory, meta, skills, resume } from '@/data/work';
+import { meta } from '@/data/work';
 import { siteName } from '@/data/shared';
 
 export const metadata: Metadata = {
@@ -15,18 +15,9 @@ export default function WorkPage() {
   return (
     <>
       <SectionHeader rootElement="h1">{meta.title}</SectionHeader>
-      <section>
-        <h2 className="sr-only">{workHistory.heading}</h2>
-        <WorkHistory />
-      </section>
-      <section>
-        <h3 className="sr-only">{resume.heading}</h3>
-        <Resume />
-      </section>
-      <section>
-        <SectionHeader>{skills.heading}</SectionHeader>
-        <Skills />
-      </section>
+      <WorkHistory headingSrOnly />
+      <Resume headingEl="h3" headingSrOnly />
+      <Skills />
     </>
   );
 }
--- src/ts/components/Resume/Resume.tsx
+++ src/ts/components/Resume/Resume.tsx
@@ -1,26 +1,30 @@
 import Divider from '../Common/Divider/Divider';
 import { resume } from '@/data/work';
 import ExpandCollapse from '../Common/ExpandCollapse/ExpandCollapse';
+import ContentCodeBlock from '../Common/ContentCodeBlock/ContentCodeBlock';
+import { ContentCodeBlockWrapperProps } from '../Common/ContentCodeBlock/types';
 
-export default function Resume() {
+export default function Resume(props: ContentCodeBlockWrapperProps) {
   return (
-    <div className="text-center">
-      <Divider />
+    <ContentCodeBlock {...props} heading={resume.heading}>
+      <div className="text-center">
+        <Divider />
 
-      <div
-        className="font-raleway text-2xl mb-8"
-        dangerouslySetInnerHTML={{ __html: resume.description }}
-      />
-
-      <ExpandCollapse buttonLabel={resume.cta} unmountOnExit={false}>
-        <iframe
-          className="w-full h-[1000px]"
-          src="/resume-adam-mornes.pdf"
-          title="Resume"
+        <div
+          className="font-raleway text-2xl mb-8"
+          dangerouslySetInnerHTML={{ __html: resume.description }}
         />
-      </ExpandCollapse>
 
-      <Divider />
-    </div>
+        <ExpandCollapse buttonLabel={resume.cta} unmountOnExit={false}>
+          <iframe
+            className="w-full h-[1000px]"
+            src="/resume-adam-mornes.pdf"
+            title="Resume"
+          />
+        </ExpandCollapse>
+
+        <Divider />
+      </div>
+    </ContentCodeBlock>
   );
 }
