a312b2ea25c6db80cb1671e203b790c6ef3f18e6
Adam Mornes <adammornes@gmail.com>
2025-06-16 13:27:55 -0400

    data formatting

--- src/data/about.ts
+++ src/data/about.ts
@@ -10,12 +10,13 @@ export const about = {
     src: profileImage.src,
   },
   description: `
-  <p>Hello, I'm Adam Mornes. I'm a Web Developer located in Flint, MI.</p>
+    <p>Hello, I'm Adam Mornes. I'm a Web Developer located in Flint, MI.</p>
 
-  <p>I have a passion for creating outstanding responsive web sites - while keeping in mind intelligent, intuitive user interface. 
-  To do so, I work to keep up with the best and newest development trends; as well as implementing those them on every project I take on.</p>
+    <p>I have a passion for creating outstanding responsive web sites - while keeping in mind intelligent, intuitive user interface. 
+    To do so, I work to keep up with the best and newest development trends; as well as implementing those them on every project I take on.</p>
 
-  <p>There is no better feeling than creating an easy to use, beautiful website that clients can be proud of.</p>`,
+    <p>There is no better feeling than creating an easy to use, beautiful website that clients can be proud of.</p>
+  `,
 };
 
 export const hobbies = {
@@ -35,66 +36,72 @@ export const journey = {
     {
       image: {
         alt: 'Lighthouse in Duluth, MN',
-        credit: `Photo by
-            <a href="https://unsplash.com/@ging_dlh?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">
-              ginger
-            </a>
-            on
-            <a href="https://unsplash.com/photos/snow-covered-lighthouse-ydrp-zMoMsg?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">
-              Unsplash
-            </a>`,
+        credit: `
+          Photo by
+          <a href="https://unsplash.com/@ging_dlh?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">
+            ginger
+          </a>
+          on
+          <a href="https://unsplash.com/photos/snow-covered-lighthouse-ydrp-zMoMsg?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">
+            Unsplash
+          </a>
+        `,
         src: duluthLighthouse.src,
       },
       description: `
-            <p>
-              Born and raised in MN, I spent the first three years of my career at 
-              <a href="https://arinet.com/ " target="_blank">ARI Network Services</a> 
-              honing my skills with HTML, CSS, and CSS theming while fighting off the cold of Duluth, MN.
-            </p>
-            <p>
-              While my time at ARI wasn&apos;t the most technically demanding, I
-              learned many important lessons about CSS Theming I now consider
-              core to my skillset.
-            </p>
+        <p>
+          Born and raised in MN, I spent the first three years of my career at 
+          <a href="https://arinet.com/ " target="_blank">ARI Network Services</a> 
+          honing my skills with HTML, CSS, and CSS theming while fighting off the cold of Duluth, MN.
+        </p>
+        <p>
+          While my time at ARI wasn&apos;t the most technically demanding, I
+          learned many important lessons about CSS Theming I now consider
+          core to my skillset.
+        </p>
       `,
     },
     {
       image: {
         alt: 'Austin, TX Skyline',
-        credit: `Photo by
-            <a href="https://unsplash.com/@mitchkmetz?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">
-              Mitchell Kmetz
-            </a>
-            on
-            <a href="https://unsplash.com/photos/man-in-black-jacket-walking-on-bridge-near-body-of-water-during-daytime-ztVcGTSD8xw?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">
-              Unsplash
-            </a>`,
+        credit: `
+          Photo by
+          <a href="https://unsplash.com/@mitchkmetz?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">
+            Mitchell Kmetz
+          </a>
+          on
+          <a href="https://unsplash.com/photos/man-in-black-jacket-walking-on-bridge-near-body-of-water-during-daytime-ztVcGTSD8xw?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">
+            Unsplash
+          </a>
+        `,
         src: austinSkyline.src,
       },
       description: `
-          <p>
-            After all that time in the frigid north, I decided to make a major
-            change. I moved to 1200 miles south to Austin, TX.
-          </p>
-          <p>
-            While in Austin, I got my next job at <a href="https://www.xcentium.com/" target="_blank">XCentium</a>. 
-            I spent the next 7 years in the wild west of contract work;
-            working on projects with technology stacks ranging from .NET and
-            JQuery to React and Next.js.
-          </p>
+        <p>
+          After all that time in the frigid north, I decided to make a major
+          change. I moved to 1200 miles south to Austin, TX.
+        </p>
+        <p>
+          While in Austin, I got my next job at <a href="https://www.xcentium.com/" target="_blank">XCentium</a>. 
+          I spent the next 7 years in the wild west of contract work;
+          working on projects with technology stacks ranging from .NET and
+          JQuery to React and Next.js.
+        </p>
       `,
     },
     {
       image: {
         alt: 'Graffiti in Flint, MI',
-        credit: `Photo by 
+        credit: `
+          Photo by 
           <a href="https://unsplash.com/@kazaks?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">
             Krišjānis Kazaks
           </a> 
           on 
           <a href="https://unsplash.com/photos/colorful-graffiti-on-the-side-of-a-building-slXpdVgW2pQ?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">
             Unsplash
-          </a>`,
+          </a>
+        `,
         src: flintGraffiti.src,
       },
       description: `
